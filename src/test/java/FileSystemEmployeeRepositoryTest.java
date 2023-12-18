a7f523a389970c1ed3cbec22ba82e7de62974a13
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-12-18 15:25:15 +0100

    added tests for parseEmployee

--- src/test/java/FileSystemEmployeeRepositoryTest.java
+++ src/test/java/FileSystemEmployeeRepositoryTest.java
@@ -1,8 +1,13 @@
+import domain.entity.Employee;
 import fileSystem.FileSystemEmployeeRepository;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -19,5 +24,26 @@ public class FileSystemEmployeeRepositoryTest {
         FileSystemEmployeeRepository repository = new FileSystemEmployeeRepository(Path.of("src/test/resources/employees.txt"));
 
         assertThat(repository.extractAllEmployees()).asList().size().isEqualTo(2);
+    }
+
+    @Test
+    void shouldParseEmployeeCorrectlyIdentifyingItsData() throws IOException {
+        Path employeeFilePath = Files.createTempFile("employee", ".txt");
+        String lastName = "Ann";
+        String firstName = "Mary";
+        String dateOfBirth = "1975/03/11";
+        String email = "mary.ann@foobar.com";
+        Files.write(employeeFilePath, Arrays.asList(
+                "last_name, first_name, date_of_birth, email",
+                lastName + ", " + firstName + ", " + dateOfBirth + ", " + email
+        ));
+
+        FileSystemEmployeeRepository repository = new FileSystemEmployeeRepository(employeeFilePath);
+        Employee employee = repository.extractAllEmployees().get(0);
+
+        assertThat(employee.lastName()).isEqualTo(lastName);
+        assertThat(employee.firstName()).isEqualTo(firstName);
+        assertThat(employee.dateOfBirth()).isEqualTo(LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("y/M/d")));
+        assertThat(employee.email()).isEqualTo(email);
     }
 }
